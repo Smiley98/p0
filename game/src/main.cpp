ac1f6aa4437c2e14ee8913f2c59b55b06cdd353d
Smiley98 <smileysofficialcinema@gmail.com>
2025-05-05 21:42:31 -0400

    Add project files.

---
+++ game/src/main.cpp
@@ -0,0 +1,41 @@
+#include "raylib.h"
+#include "raymath.h"
+
+void GameInit()
+{
+    SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_RESIZABLE);
+    InitWindow(800, 800, "Game");
+    SetTargetFPS(144);
+}
+
+void GameCleanup()
+{
+    CloseWindow();
+}
+
+void GameUpdate(float dt)
+{
+
+}
+
+void GameDraw()
+{
+    BeginDrawing();
+    ClearBackground(DARKGRAY);
+    DrawText("Hello World!", 10, 10, 20, RED);
+    EndDrawing();
+}
+
+int main()
+{
+    GameInit();
+
+    while (!WindowShouldClose())
+    {
+        GameUpdate(GetFrameTime());
+        GameDraw();
+    }
+
+    GameCleanup();
+    return 0;
+}

85d6a8dc87c6b5181998286248d5808fd8b1f510
Jesse McCormack <61964089+JesseSound@users.noreply.github.com>
2025-06-17 17:40:27 -0400

    Update GroundScene.h with proper vars

--- game/src/GroundScene.h
+++ game/src/GroundScene.h
@@ -9,11 +9,11 @@ public:
 	RenderTexture2D hexGridTarget;
 
 	// Shader uniforms
-	int timeLoc, resLoc, fgColLoc, bgColLoc, hexResLoc, hexThicknessLoc;
-	Vector3 fgColor = { 1.0f, 1.0f, 1.0f };
-	Vector3 bgColor = { 0.0f, 0.0f, 0.0f };
-	float hexRes = 10.0f;
-	float hexThickness = 0.1f;
+	int time_loc, res_loc, fg_col_loc, bg_col_loc, hex_res_loc, hex_thickness_loc;
+	Vector3 fg_color = { 1.0f, 1.0f, 1.0f };
+	Vector3 bg_color = { 0.0f, 0.0f, 0.0f };
+	float hex_res = 10.0f;
+	float hex_thickness = 0.1f;
 	float time = 0.0f;
 	void OnLoad() final;
 	void OnUnload() final;
